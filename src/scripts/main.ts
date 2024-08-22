14af1e92b15aac3183a2d7bf88e976ded8a44f38
luuizz <luu.izz@hotmail.com>
2024-08-21 22:30:33 -0300

    feature: inserido aos

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,6 +1,14 @@
 import "./../scss/main.scss";
 import { slidesTestimonials } from "./slide.js";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 document.addEventListener("DOMContentLoaded", () => {
   slidesTestimonials.init();
+});
+
+AOS.init({
+  duration: 1000,
+  once: true,
+  disable: "mobile",
 });
